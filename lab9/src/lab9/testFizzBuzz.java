2e4e23d939560936e99d5e63665a6615ffe8ee5b
“kanjanaloet” <kanjana_loet@kkumail.com>
2019-10-08 22:35:42 +0700

    input2output2

--- lab9/src/lab9/testFizzBuzz.java
+++ lab9/src/lab9/testFizzBuzz.java
@@ -9,10 +9,14 @@ class testFizzBuzz {
 	Main m = new Main();
 	
 	String i1 = m.getInt(1);
+	String i2 = m.getInt(2);
 	
 	@Test
 	void intput_1_output_1() {
 		assertEquals( "1", i1);	
 	}
-
+	@Test
+	void intput_2_output_2() {
+		assertEquals( "2", i2);	
+	}
 }
