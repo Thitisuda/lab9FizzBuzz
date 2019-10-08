2c5033f164f8fb1ef0675a1d4f6d97a2b5e53bca
“kanjanaloet” <kanjana_loet@kkumail.com>
2019-10-08 22:41:39 +0700

    input4output4

--- lab9/src/lab9/testFizzBuzz.java
+++ lab9/src/lab9/testFizzBuzz.java
@@ -11,6 +11,7 @@ class testFizzBuzz {
 	String i1 = m.getInt(1);
 	String i2 = m.getInt(2);
 	String i3 = m.getInt(3);
+	String i4 = m.getInt(4);
 	
 	@Test
 	void intput_1_output_1() {
@@ -25,5 +26,9 @@ class testFizzBuzz {
 	@Test
 	void intput_3_output_Fizz() {
 		assertEquals( "Fizz", i3);	
+	}
+	@Test
+	void intput_4_output_4() {
+		assertEquals( "4", i4);	
 	}
 }
